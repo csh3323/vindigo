697c80f00e36dd68eb930192bde755329d7cb429
macjuul <julian.exodius@gmail.com>
2021-05-16 04:54:31 +0200

    feat: further work on initial setup

--- packages/vindigo-server/src/index.ts
+++ packages/vindigo-server/src/index.ts
@@ -1,7 +1,18 @@
-console.log('Anus');
-console.log('Anus');
-console.log('Anus');
-console.log('Anus');
-console.log('Anus');
-console.log('Anus');
-console.log('Anus');
+import { VindigoServer } from "./server";
+import ON_DEATH from 'death';
+import consola from "consola";
+
+const vindigo = new VindigoServer();
+
+export { vindigo };
+
+try {
+	vindigo.start();
+
+	ON_DEATH(() => {
+		vindigo.stop();
+	});
+} catch(err) {
+	consola.error(err);
+	process.exit(1);
+}
---
+++ packages/vindigo-server/src/server.ts
@@ -0,0 +1,16 @@
+import consola from "consola";
+
+/**
+ * The main Vindigo server manager
+ */
+export class VindigoServer {
+
+	public start() {
+		consola.info("Starting Vindigo server");
+	}
+
+	public stop() {
+		consola.info("Stopping Vindigo server");
+	}
+
+}
