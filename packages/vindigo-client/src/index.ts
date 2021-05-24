dc4b42552c2fcfc13d06d79b9917d975c2f8adc8
Beau <beaudenheijer@gmail.com>
2021-05-24 15:48:42 +0200

    added routes and configured router

--- packages/vindigo-client/src/index.ts
+++ packages/vindigo-client/src/index.ts
@@ -7,18 +7,34 @@ import Router from 'vue-router';
 import Store from 'vuex';
 import Vue from 'vue';
 import dayjs from 'dayjs';
+import VueRouter from 'vue-router';
 
 // Configure packages
 dayjs.locale('nl-nl');
 dayjs.extend(RelativeTime);
 
-Vue.use(Oruga);
+// Configure routes
+const routes = [
+	{ path: '/', component: () => import('./view/Home.vue') },
+	{ path: '/list', component: null },
+	{ path: '/edit', component: null },
+	{ path: '/restore', component: null },
+	{ path: '/organization', component: null }
+];
+
+const router = new VueRouter({
+	routes: routes,
+	mode: 'history'
+});
+
+//Vue.use(Oruga);
 Vue.use(Router);
 Vue.use(Store);
 
 // Instantiate the application
 const vue = new Vue({
 	el: '#app',
+	router,
 	render: (m) => {
 		return m(App);
 	}
