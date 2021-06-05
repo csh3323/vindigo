e9ffcc0562cfdea9bbbb96971e2d18f06e338cc6
macjuul <julian.exodius@gmail.com>
2021-06-06 00:15:44 +0200

    feat(client): add store capabilities

--- packages/vindigo-client/src/registry/state.ts
+++ packages/vindigo-client/src/registry/state.ts
@@ -1,6 +1,26 @@
+import { store } from "..";
+import { storeActions } from "./actions";
+import { storeMutations } from "./mutations";
+
+/**
+ * The root state for the vindigo client
+ */
+export interface RootState {
+	test: string;
+}
+
 /**
  * Register all store state
  */
 export function registerState() {
+
+	store.defineNamespace({
+		id: 'core',
+		mutations: storeMutations,
+		actions: storeActions,
+		state: {
+			test: ''
+		}
+	});
 
 }
