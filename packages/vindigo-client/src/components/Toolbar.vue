1eb14a1f7b0777cd2dd86e34ea6a9a79fbcd706b
Beau <beaudenheijer@gmail.com>
2021-07-08 23:04:09 +0200

    feat(client): added account menu

--- packages/vindigo-client/src/components/Toolbar.vue
+++ packages/vindigo-client/src/components/Toolbar.vue
@@ -55,13 +55,79 @@
 					/>
 				</div>
 			</template>
-			<div class="toolbar__account-menu rounded-2xl p-3 w-60">
-				<w-button
-					round
-					@click="$store.dispatch('signOut')"
-				>
-					Sign out
-				</w-button>
+			<div class="toolbar__account-menu bg-white rounded-xl py-3 w-60">
+				<p class="text-center mb-3">
+					Your Acccount
+				</p>
+				<w-divider />
+				<div class="toolbar-list rounded-b-2xl">
+					<router-link 
+						to="/profile/10" class="toolbar-list__item" 
+						tag="div"
+					>
+						<w-icon size="1.1rem">
+							mdi mdi-account
+						</w-icon>
+						Your profile
+					</router-link>
+					<router-link 
+						to="" class="toolbar-list__item"
+						tag="div"
+					>
+						<w-icon size="1.1rem">
+							mdi mdi-animation
+						</w-icon>
+						Your projects
+					</router-link>
+					<router-link 
+						to="" class="toolbar-list__item"
+						tag="div"
+					>
+						<w-icon size="1.1rem">
+							mdi mdi-briefcase
+						</w-icon>
+						Your teams
+					</router-link>
+					<router-link 
+						to="" class="toolbar-list__item"
+						tag="div"
+					>
+						<w-icon size="1.1rem">
+							mdi mdi-google-analytics
+						</w-icon>
+						Your activity
+					</router-link>
+					<w-divider />
+					<router-link 
+						to="" class="toolbar-list__item"
+						tag="div"
+					>
+						<w-icon size="1.1rem">
+							mdi mdi-help-circle-outline
+						</w-icon>
+						Help
+					</router-link>
+					<router-link 
+						to="" class="toolbar-list__item"
+						tag="div"
+					>
+						<w-icon size="1.1rem">
+							mdi mdi-cog-outline
+						</w-icon>
+						Settings
+					</router-link>
+					<w-divider />
+					<router-link
+						to="" 
+						class="toolbar-list__item text-red-400" 
+						tag="div" @click="$store.dispatch('signOut')"
+					>
+						<w-icon size="1.1rem">
+							mdi mdi-logout
+						</w-icon>
+						Sign out
+					</router-link>
+				</div>
 			</div>
 		</w-menu>
 	</header>
@@ -78,7 +144,9 @@ export default Vue.extend({
 			type: Object as PropType<IconButton[]>,
 		},
 	},
+	data: () => ({
 
+	}),
 	computed: {
 		logoUrl(): boolean {
 			return this.$store.state.isDark
@@ -90,6 +158,25 @@ export default Vue.extend({
 </script>
 
 <style lang="postcss">
+
+.toolbar-list {
+
+	&__item {
+
+		@apply px-3 py-1 cursor-pointer bg-white;
+
+		transition: .15s background-color ease-in-out;
+
+		&:hover {
+			@apply bg-gray-100;
+		}
+
+		.w-icon {
+			@apply mt-[-3px] mr-1;
+		}
+	}
+}
+
 .toolbar {
 	&__divider {
 		background-color: #e1e1e1;
