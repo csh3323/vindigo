4434b21c2258b0a97cd5e33558afc68026eae24c
Beau <beaudenheijer@gmail.com>
2021-07-25 22:51:13 +0200

    feat(server): added search query graphql

--- packages/vindigo-server/src/schemas/projects/resolvers/query.ts
+++ packages/vindigo-server/src/schemas/projects/resolvers/query.ts
@@ -4,6 +4,9 @@ import { FindConditions } from "typeorm";
 import { GraphQLResolvers } from "../../../http";
 import { Project } from "../../../models/project";
 import { parseTakeSize } from "../../../util/http";
+import { Team } from "../../../models/team";
+import { User } from "../../../models/user";
+import { Like } from "typeorm";
 
 export default {
 	projects: async (_, { mode, skip, take }, ctx) => {
@@ -40,5 +43,31 @@ export default {
 			skip: skip,
 			take: parseTakeSize(take, 50)
 		});
-	}
+	},
+	search: async(_, { query }) => {
+
+		const search = { 
+			name: Like(`%${query}%`) 
+		};
+
+		const projectTask = Project.find({
+			where: search
+		});
+
+		const teamsTask = Team.find({
+			where: search
+		});
+
+		const userTask = User.find({
+			where: search
+		});
+
+		const [projects, teams, users] = await Promise.all([
+			projectTask,
+			teamsTask,
+			userTask
+		]);
+
+		return { projects, teams, users };
+	}	
 } as GraphQLResolvers;
